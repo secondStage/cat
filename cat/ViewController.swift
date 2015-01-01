b2e33aace18704a0587d21ddb6067e517ab17722
Mafuyu Kamono <kamomafu2181@yahoo.co.jp>
2015-01-01 16:14:16 +0900

    add 猫の画像をarrayで保持

--- cat/ViewController.swift
+++ cat/ViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let arrayCatImg     = ["cat01.png", "cat02.png"]
+    let arrayCatEeeImg  = ["cateee01.png", "cateee02.png"]
+    let arrayCatNikoImg = ["catniko01.png", "catniko02.png"]
+    let arrayCatGoroImg = ["catgoro01.png", "catgoro02.png"]
+    let arrayCatSadImg  = ["catsad01.png", "catsad02.png"]
+    let catSleepImg = "catsleep.png"
+    let catSmileImg = "catsmile.png"
+    
+    var countCatEee  = 0
+    var countCatGoro = 0
+    var countCatSad  = 0
 
     @IBOutlet weak var catImage: UIImageView!
     
@@ -23,22 +34,34 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tapEeeCat(sender: AnyObject) {
-        let img = UIImage(named: "cateee01.png")
+        var selector = countCatEee % 2
+        var getImg   = arrayCatEeeImg[selector]
+
+        var img:  UIImage? = UIImage(named: getImg)
         catImage.image = img
+        countCatEee += 1
     }
     
     @IBAction func tapGorogoroCat(sender: AnyObject) {
-        let img = UIImage(named: "catgoro01.png")
+        var selector = countCatGoro % 2
+        var getImg   = arrayCatGoroImg[selector]
+        
+        var img:  UIImage? = UIImage(named: getImg)
         catImage.image = img
+        countCatGoro += 1
     }
     
     @IBAction func tapSadCat(sender: AnyObject) {
-        let img = UIImage(named: "catsad01.png")
+        var selector = countCatSad % 2
+        var getImg   = arrayCatSadImg[selector]
+        
+        var img:  UIImage? = UIImage(named: getImg)
         catImage.image = img
+        countCatSad += 1
     }
 
     @IBAction func tapSleepCat(sender: AnyObject) {
-        let img = UIImage(named: "catsleep.png")
+        let img = UIImage(named: catSleepImg)
         catImage.image = img
     }
 }
